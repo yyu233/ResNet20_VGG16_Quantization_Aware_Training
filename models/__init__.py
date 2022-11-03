a7249b5f3e061061619152c3ddacc8d05fea7cbc
yyu233 <yizheng.yu2018@gmail.com>
2022-11-03 09:59:27 -0700

    Initial commit

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from .resnet import *
+from .vgg import *
+from .vgg_quant import *
+from .resnet_quant import *
+from .cifar import *
---
+++ models/cifar.py
@@ -0,0 +1,71 @@
+
+
+import torch
+import torch.nn as nn
+import math
+
+
+class CIFAR(nn.Module):
+    def __init__(self, features, n_channel, num_classes):
+        super(CIFAR, self).__init__()
+        assert isinstance(features, nn.Sequential), type(features)
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Linear(n_channel, num_classes)
+        )
+        print(self.features)
+        print(self.classifier)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+            
+    
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for i, v in enumerate(cfg):
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        elif v == 'F':  # First layer 
+            padding = v[1] if isinstance(v, tuple) else 1
+            out_channels = 128 
+            conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=padding)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(out_channels, affine=False), nn.ReLU()]
+            else:
+                layers += [conv2d, nn.ReLU()]
+            in_channels = out_channels
+        else:
+            padding = v[1] if isinstance(v, tuple) else 1
+            out_channels = v[0] if isinstance(v, tuple) else v
+            
+            #### Instead of standard Conv2d function, we are using Quantized version in quant_layer.py
+            conv2d = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=padding)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(out_channels, affine=False), nn.ReLU()]
+            else:
+                layers += [conv2d, nn.ReLU()]
+            in_channels = out_channels
+    return nn.Sequential(*layers)
+
+
+def cifar10(n_channel=128, pretrained=None, **kwargs):
+    #cfg = [n_channel, n_channel, 'M', 2*n_channel, 2*n_channel, 'M', 4*n_channel, 4*n_channel, 'M', (8*n_channel, 0), 'M']
+    cfg = ['F', n_channel, 'M', 2*n_channel, 2*n_channel, 'M', 4*n_channel, 4*n_channel, 'M', (8*n_channel, 0), 'M']
+    layers = make_layers(cfg, batch_norm=True)
+    model = CIFAR(layers, n_channel=8*n_channel, num_classes=10)
+    if pretrained is not None:
+        m = model_zoo.load_url(model_urls['cifar10'])
+        state_dict = m.state_dict() if isinstance(m, nn.Module) else m
+        assert isinstance(state_dict, (dict, OrderedDict)), type(state_dict)
+        model.load_state_dict(state_dict)  
+        
+    return model
+
+
+
+
+
